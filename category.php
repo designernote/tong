92c2ed53c6f32ffc4438e40d11a634a65f443490
sunny <kkuk79@naver.com>
2024-12-24 17:27:22 +0900

    1224_01

---
+++ category.php
@@ -0,0 +1,132 @@
+<html lang="ko">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1 minimum-scale=1, maximum-scale=1" />
+    <title></title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/swiper.css" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body>
+    <section class="wrap">
+      <header class="header">
+        <h1 class="back"><a href="index.html">back</a></h1>
+        <h2 class="page_title">부동산</h2>
+        <!-- <a href="#" class="btn_sideMenu">메뉴</a> -->
+      </header>
+      <main role="main">
+        <div class="container">
+          <section class="item_list">
+            <div class="item">
+              <div class="title">
+                <p>프리미엄</p>
+                <span>모두 보기</span>
+              </div>
+              <ul class="ranking_list">
+                <li>
+                  <div>
+                    <img src="./assets/images/ex_img.png" class="thum_img" alt="">
+                    <div class="inner_txt">
+                      <p>부동산 / 금나와라부동산</p>
+                      <small>서울시 동작구 양녕로 220, 상가 103호</small>
+                      <span>이사지원금 (20만원상당)외 1건</span>
+                    </div>
+                  </div>
+                  <div class="average">
+                    <span>8.2</span>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <img src="./assets/images/ex_img.png" class="thum_img" alt="">
+                    <div class="inner_txt">
+                      <p>부동산 / 금나와라부동산</p>
+                      <small>서울시 동작구 양녕로 220, 상가 103호</small>
+                      <span>이사지원금 (20만원상당)외 1건</span>
+                    </div>
+                  </div>
+                  <div class="average">
+                    <span>8.2</span>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <img src="./assets/images/ex_img.png" class="thum_img" alt="">
+                    <div class="inner_txt">
+                      <p>부동산 / 금나와라부동산</p>
+                      <small>서울시 동작구 양녕로 220, 상가 103호</small>
+                      <span>이사지원금 (20만원상당)외 1건</span>
+                    </div>
+                  </div>
+                  <div class="average">
+                    <span>8.2</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="item">
+              <div class="capsule">
+                <button class="on">전체</button>
+                <button>검색순</button>
+                <button>AI 혜택순</button>
+              </div>
+              <div class="title">
+                <p>AI 혜택순위</p>
+                <span>모두 보기</span>
+              </div>
+              <ul class="ranking_list">
+                <li>
+                  <div>
+                    <img src="./assets/images/ex_img.png" class="thum_img" alt="">
+                    <div class="inner_txt">
+                      <p>부동산 / 금나와라부동산</p>
+                      <small>서울시 동작구 양녕로 220, 상가 103호</small>
+                      <span>이사지원금 (20만원상당)외 1건</span>
+                    </div>
+                  </div>
+                  <div class="average">
+                    <span>8.2</span>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <img src="./assets/images/ex_img.png" class="thum_img" alt="">
+                    <div class="inner_txt">
+                      <p>부동산 / 금나와라부동산</p>
+                      <small>서울시 동작구 양녕로 220, 상가 103호</small>
+                      <span>이사지원금 (20만원상당)외 1건</span>
+                    </div>
+                  </div>
+                  <div class="average">
+                    <span>8.2</span>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <img src="./assets/images/ex_img.png" class="thum_img" alt="">
+                    <div class="inner_txt">
+                      <p>부동산 / 금나와라부동산</p>
+                      <small>서울시 동작구 양녕로 220, 상가 103호</small>
+                      <span>이사지원금 (20만원상당)외 1건</span>
+                    </div>
+                  </div>
+                  <div class="average">
+                    <span>8.2</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </main>
+      <?php
+// footer.php 파일 포함
+include 'include/footer.php';
+?>
---
+++ storeDetail.php
@@ -0,0 +1,45 @@
+<html lang="ko">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1 minimum-scale=1, maximum-scale=1" />
+    <title></title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/swiper.css" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body>
+    <section class="wrap">
+      <header class="header">
+        <h1 class="back"><a href="index.html">back</a></h1>
+        <h2 class="page_title"></h2>
+        <!-- <a href="#" class="btn_sideMenu">메뉴</a> -->
+      </header>
+      <main role="main">
+        <div class="container">
+          <section>
+            <div class="detail_view">
+              <div class="img_container">
+                <img src="./assets/images/ex_img3.png" class="thum_img3" alt="">
+              </div>
+              <p>금나와라공인중계사<small>부동산</small></p>
+              <span>AI혜택 점수 <em>4.8</em></span>
+            </div>
+            <ul>
+              <li>
+                전화
+              </li>
+            </ul>
+          </section>
+        </div>
+      </main>
+      <?php
+// footer.php 파일 포함
+include 'include/footer.php';
+?>
